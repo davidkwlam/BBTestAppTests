16caa0aa3ba87c760c8847383e056328369ef07e
David Lam <david@buddybuild.com>
2017-03-29 14:45:31 -0700

    Update SDK

--- BBTestAppTestsUITests/BBTestAppTestsUITests.swift
+++ BBTestAppTestsUITests/BBTestAppTestsUITests.swift
@@ -22,12 +22,12 @@ class BBTestAppTestsUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         
-        BuddyBuildSDK.startUITestsVideoRecording()
+        BuddyBuildSDK.startUITests()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        BuddyBuildSDK.stopUITestsVideoRecording()
+        BuddyBuildSDK.stopUITests()
         super.tearDown()
     }
     
